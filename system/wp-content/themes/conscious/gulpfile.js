93d8f3e5fb3924eff692716acca0febefa033868
Ryuichi Komeda <raysbacker@gmail.com>
2016-01-13 21:12:23 +0900

    Refactoring script

--- system/wp-content/themes/conscious/gulpfile.js
+++ system/wp-content/themes/conscious/gulpfile.js
@@ -1,15 +1,21 @@
 'use strict';
 
-var gulp        = require('gulp');
-var compass     = require('gulp-compass');
-var SCSS_FILE   = './scss/**/*.scss';
+var gulp    = require('gulp');
+var compass = require('gulp-compass');
+var rename  = require('gulp-rename');
+var uglify  = require('gulp-uglify');
 
+var config = {
+  style: {
+    src: './scss/**/*.scss'
+  },
+  script: {
+    src: './js/main.js'
+  }
+}
 
-/*
- * Compass task
- */
 gulp.task('compass',function(){
-  gulp.src([SCSS_FILE])
+  gulp.src(config.style.src)
     .pipe(compass({
       config_file : 'config.rb',
       comments : false,
@@ -18,12 +24,18 @@ gulp.task('compass',function(){
     }));
 });
 
+gulp.task('script', function () {
+  gulp.src(config.script.src)
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(uglify())
+    .pipe(gulp.dest('js'));
+});
 
-/*
- * Watch task
- */
 gulp.task('watch',function(){
-  gulp.watch(SCSS_FILE, function(event){
+  gulp.watch(config.style.src, function(event){
     gulp.run('compass');
+  });
+  gulp.watch(config.script.src, function(event){
+    gulp.run('script');
   });
 });
