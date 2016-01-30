005aafc09f2669b628060c241d17eb961006a27b
Ryuichi Komeda <raysbacker@gmail.com>
2016-01-30 15:59:12 +0900

    Change Tag Manager

--- system/wp-content/themes/conscious/header.php
+++ system/wp-content/themes/conscious/header.php
@@ -14,13 +14,13 @@
 <body <?php body_class(); ?>>
 
 <!-- Google Tag Manager -->
-<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-WCMLVP"
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-MH724P"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WCMLVP');</script>
+})(window,document,'script','dataLayer','GTM-MH724P');</script>
 <!-- End Google Tag Manager -->
 
 <div id="fb-root"></div>
